509ed44c9482e07611a5489cee896e000c365273
CY Kalu <cy23@tutanota.com>
2021-11-25 10:59:24 +0000

    Add more test cases

--- src/__tests__/stringSimilarity.test.js
+++ src/__tests__/stringSimilarity.test.js
@@ -7,9 +7,15 @@ describe('stringSimilarity', () => {
 
   it('should return less than 1 when both strings have one letter difference', () => {
     expect(stringSimilarity('apple', 'apples')).toBeLessThan(1);
+    expect(stringSimilarity('apple', 'apples')).toBeGreaterThan(0);
   });
 
   it('should return 0 when strings have no common letters', () => {
     expect(stringSimilarity('hunger', 'loyalty')).toBe(0);
+  });
+
+  it('should return between 0 and 1 when strings have some common letters', () => {
+    expect(stringSimilarity('apple', 'app')).toBeLessThan(1);
+    expect(stringSimilarity('apple', 'app')).toBeGreaterThan(0);
   });
 });
